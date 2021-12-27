fadb4c40e4f1103c1bd88db79896ac4f90cea2b9
philipp <philipp.armingeon@googlemail.com>
2021-12-27 16:00:15 +0100

    Added full functionality

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -7,22 +7,42 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 	"strings"
+	"time"
 )
 
-func play(quiz *csv.Reader) {
+func exit(correct, total int) {
+	fmt.Printf("\nCongratulations! You answered %d out of %d questions correctly!\n", correct, total)
+	os.Exit(0)
+}
+
+func play(quiz *csv.Reader, limit int, total int) {
 	counter := 0
 	i := 1
+	start := time.Now()
+	go func() {
+		for {
+			if math.Floor(time.Since(start).Seconds()) == float64(limit) {
+				// fmt.Println("LIMIT REACHED")
+				exit(counter, total)
+			}
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
 	for {
 		record, err := quiz.Read()
 		if err == io.EOF {
-			fmt.Printf("Congratulations! You answered %d out of %d questions correctly!\n", counter, i-1)
-			break
+			if err != nil {
+				log.Fatal(err)
+			}
+			exit(counter, total)
 		}
 
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("PLAY FUNCTION ", err)
 		}
 		question := record[0]
 		solution := record[1]
@@ -40,23 +60,35 @@ func play(quiz *csv.Reader) {
 			fmt.Println("Correct!")
 			counter++
 		}
+
 		i++
 	}
 }
 
 func main() {
-	csvFile := flag.String("csv", "default.csv", "a csv file in the format of 'question, answer' (default \"problems.csv\")")
-	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds (default 30)")
+	csvFile := flag.String("csv", "csv/default.csv", "a csv file in the format of 'question, answer'")
+	limit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
-	fmt.Println("LIMIT: ", *timeLimit)
-
+	// second file Reader in order to get total number of questions
 	file, err := os.Open(*csvFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	f, err := os.Open(*csvFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	quiz := csv.NewReader(file)
-	play(quiz)
+	q := csv.NewReader(f)
+
+	questions, err := q.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	total := len(questions)
+	play(quiz, *limit, total)
 }
